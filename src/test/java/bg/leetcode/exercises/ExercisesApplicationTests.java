ab72bc13485321ffe86a2796b163f3f0f815bc51
Ivaylo Tenev <i.tenev@tinqin.com>
2020-03-19 12:04:39 +0200

    init

---
+++ src/test/java/bg/leetcode/exercises/ExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.leetcode.exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
