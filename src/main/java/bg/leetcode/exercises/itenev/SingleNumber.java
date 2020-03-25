658a61d085d0fba7877af49cd1bd7a24401a33bf
almanaha <ivailotenevg@gmail.com>
2020-03-25 16:53:12 +0200

    Solution for 136. Single Number

---
+++ src/main/java/bg/leetcode/exercises/itenev/SingleNumber.java
@@ -0,0 +1,41 @@
+package bg.leetcode.exercises.itenev;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ * <p>
+ * Your algorithm should have a linear runtime complexity.
+ * Could you implement it without using extra memory?
+ * <p>
+ * Input: [2,2,1]
+ * Output: 1
+ */
+public class SingleNumber {
+
+    /** Space complexity : O(1) */
+    public static int singleNumberWithXOR(int[] nums) {
+        int a = 0;
+        for (int i : nums) {
+            a ^= i;
+        }
+        return a;
+    }
+
+    /** Space complexity : O(n + n) = O(n)O(n+n)=O(n). set needs space for the elements in nums*/
+    public static int singleNumber(int[] nums) {
+        int sumOfSet = 0;
+        int sumOfNums = 0;
+        Set<Integer> set = new HashSet();
+
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+                sumOfSet += num;
+            }
+            sumOfNums += num;
+        }
+        return 2 * sumOfSet - sumOfNums;
+    }
+}
