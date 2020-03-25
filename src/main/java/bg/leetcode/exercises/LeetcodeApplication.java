33c9f6d44b26497e87cc3ca9018ce81818366407
almanaha <ivailotenevg@gmail.com>
2020-03-25 15:47:40 +0200

    Solution for 200. Number of Islands

--- src/main/java/bg/leetcode/exercises/LeetcodeApplication.java
+++ src/main/java/bg/leetcode/exercises/LeetcodeApplication.java
@@ -3,7 +3,7 @@ package bg.leetcode.exercises;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExercisesApplication {
+public class LeetcodeApplication {
 
 	public static void main(String[] args) {
 
