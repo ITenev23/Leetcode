6953011af471a6c3d51075514ccab6ecc971109f
almanaha <ivailotenevg@gmail.com>
2020-03-26 11:06:48 +0200

    242. Valid Anagram

---
+++ src/main/java/bg/leetcode/exercises/itenev/ValidAnagram.java
@@ -0,0 +1,42 @@
+package bg.leetcode.exercises.itenev;
+
+import java.util.Arrays;
+
+/**
+ * Given two strings s and t ,
+ * write a function to determine if t is an anagram of s.
+ * <p>
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ */
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] counts = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            //a -> 97 in ascii table
+            counts[s.charAt(i) - 'a']++;
+            counts[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : counts) {
+            if (count != 0) return false;
+        }
+        return true;
+    }
+
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+
+}
