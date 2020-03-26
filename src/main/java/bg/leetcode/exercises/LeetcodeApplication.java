b1a60d42883593bd78fc05639119510130b4d691
almanaha <ivailotenevg@gmail.com>
2020-03-26 12:15:06 +0200

    7. Reverse Integer

--- src/main/java/bg/leetcode/exercises/LeetcodeApplication.java
+++ src/main/java/bg/leetcode/exercises/LeetcodeApplication.java
@@ -1,12 +1,14 @@
 package bg.leetcode.exercises;
 
+import bg.leetcode.exercises.itenev.ReverseInteger;
+import bg.leetcode.exercises.itenev.string.ReverseString;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeetcodeApplication {
 
 	public static void main(String[] args) {
-
+		System.out.println(ReverseInteger.reverse(-123));
 	}
 
 }
