867f918cfc988b1d1b6b0b9b899b202dbd9e85de
almanaha <ivailotenevg@gmail.com>
2020-03-26 21:07:53 +0200

    346 Moving Average from Data Stream

---
+++ src/main/java/bg/leetcode/exercises/itenev/MovingAverageFromDataStream.java
@@ -0,0 +1,40 @@
+package bg.leetcode.exercises.itenev;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Calculate the moving average of all ints in the sliding window
+ *
+ * MovingAverage m = MovingAverage(3);
+ * m.next(1) = 1;
+ * m.next(10) = (1 + 10) / 2;
+ * m.next(3) = (1 + 10 + 3) / 3;
+ * m.next(5) = (10 + 3 + 5) / 3;
+ * remove the first one from the queue(1), because maxSize is 3
+ */
+public class MovingAverageFromDataStream {
+
+    private static class MovingAverage {
+        private Queue<Integer> queue;
+        private int maxSize;
+        private double sum;
+
+        public MovingAverage(int size) {
+            this.queue = new LinkedList<>();
+            this.maxSize = size;
+            this.sum = 0.0;
+        }
+
+        public double next(int val) {
+            if (this.queue.size() == this.maxSize)
+                this.sum -= queue.remove();
+
+            this.queue.add(val);
+            this.sum += val;
+
+            return this.sum / this.queue.size();
+        }
+    }
+
+}
