aadcbec451bb1f11b1e0f20a87f73215c66a1877
almanaha <ivailotenevg@gmail.com>
2020-03-27 12:36:30 +0200

    145. Binary Tree Postorder Traversal

---
+++ src/main/java/bg/leetcode/exercises/itenev/tree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,55 @@
+package bg.leetcode.exercises.itenev.tree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ * <p>
+ * Input: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * Output: [3,2,1]
+ */
+public class BinaryTreePostorderTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(0, node.val);
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+
+        return result;
+    }
+
+}
