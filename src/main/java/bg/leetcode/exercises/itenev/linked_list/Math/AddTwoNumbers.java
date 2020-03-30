52e6e7df5f20711bbaf03557c0df35b65b88c43a
almanaha <ivailotenevg@gmail.com>
2020-03-30 11:00:16 +0300

    2. Add Two Numbers

---
+++ src/main/java/bg/leetcode/exercises/itenev/linked_list/Math/AddTwoNumbers.java
@@ -0,0 +1,52 @@
+package bg.leetcode.exercises.itenev.linked_list.Math;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+public class AddTwoNumbers {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            //in case of the nodes is null
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = carry + x + y;
+            carry = sum / 10;
+
+            head.next = new ListNode(sum % 10);
+            head = head.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if (carry > 0)
+            head.next = new ListNode(carry);
+
+        return dummy.next;
+
+    }
+}
