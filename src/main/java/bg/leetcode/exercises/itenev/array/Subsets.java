4cf0dc725fd4827a0d4e29c12df585c511acb2cd
almanaha <ivailotenevg@gmail.com>
2020-03-31 15:06:40 +0300

    78. Subsets

---
+++ src/main/java/bg/leetcode/exercises/itenev/array/Subsets.java
@@ -0,0 +1,82 @@
+package bg.leetcode.exercises.itenev.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
+ * <p>
+ * Note: The solution set must not contain duplicate subsets.
+ * <p>
+ * Input: nums = [1,2,3]
+ * Output:
+ * [
+ * [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ */
+public class Subsets {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        backtrack(nums, subsets, 0, new ArrayList<>());
+        return subsets;
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> subsets, int index, ArrayList<Integer> current) {
+        subsets.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtrack(nums, subsets, i + 1, current);
+            current.remove(current.size() - 1);
+        }
+    }
+
+    /**********************************************************************/
+
+
+    public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        output.add(new ArrayList<>());
+
+        for (int num : nums) {
+            List<List<Integer>> newSubsets = new ArrayList<>();
+            for (List<Integer> curr : output) {
+                newSubsets.add(new ArrayList<>(curr) {{
+                    add(num);
+                }});
+            }
+            output.addAll(newSubsets);
+        }
+        return output;
+    }
+
+    /**********************************************************************/
+
+
+
+    public List<List<Integer>> subsets3(int[] nums) {
+        List<List<Integer>> output = new ArrayList();
+        int n = nums.length;
+
+        for (int i = (int)Math.pow(2, n); i < (int)Math.pow(2, n + 1); ++i) {
+            // generate bitmask, from 0..00 to 1..11
+            String bitmask = Integer.toBinaryString(i).substring(1);
+
+            // append subset corresponding to that bitmask
+            List<Integer> curr = new ArrayList();
+            for (int j = 0; j < n; ++j) {
+                if (bitmask.charAt(j) == '1') curr.add(nums[j]);
+            }
+            output.add(curr);
+        }
+        return output;
+    }
+
+}
