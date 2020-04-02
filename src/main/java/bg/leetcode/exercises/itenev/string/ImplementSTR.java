7b58324898b66a96b69ced92611dc219634441b2
almanaha <ivailotenevg@gmail.com>
2020-04-02 11:08:16 +0300

    28. Implement strStr()

---
+++ src/main/java/bg/leetcode/exercises/itenev/string/ImplementSTR.java
@@ -0,0 +1,54 @@
+package bg.leetcode.exercises.itenev.string;
+
+/**
+ * Implement strStr().
+ *
+ * Return the index of the first occurrence of needle in haystack,
+ * or -1 if needle is not part of haystack.
+ *
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ *
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ */
+public class ImplementSTR {
+
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length())
+                    return i;
+                if (i + j == haystack.length())
+                    return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j))
+                    break;
+            }
+        }
+    }
+
+    /***************************************/
+
+    public int strStr2(String haystack, String needle) {
+        if (needle.length() == 0)
+            return 0;
+        if (haystack.length() == 0)
+            return -1;
+
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (i + needle.length() > haystack.length())
+                break;
+
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i+j) != needle.charAt(j))
+                    break;
+                if (j == needle.length()-1)
+                    return i;
+            }
+        }
+
+        return -1;
+    }
+
+}
